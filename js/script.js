36aca10e349889dcc2bac0bde82477775cd270cd
midigo0411 <midigo041@gmail.com>
2021-04-12 00:06:51 +0300

    adding arrays

--- js/script.js
+++ js/script.js
@@ -44,3 +44,7 @@ function checkDayOfWeek(){
      checkGender();
      console.log("The function runs");//Test chackDayOfWeek function
 }
+
+//arrays
+let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
