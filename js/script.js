f3bf85b43b6cc145c6d8caa0e88baca1fdf7cb6f
midigo0411 <midigo041@gmail.com>
2021-04-11 23:57:20 +0300

    getting the input function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 var century, year, month, dayOfMonth, dayOfWeek, day;
+//gettting the input
+function getInput(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthday").value);
+  
+  
+    if(century == ""){
+      alert("Input the correct gender");
+      return false;
+    }else if (year == ""){
+      alert("Input the correct year");
+      return false;
+    }else if (month == ""){
+      alert("Input the correct month");
+      return false;
+    }else if(dayOfMonth == ""){
+      alert("input the correct date");
+      return false;
+    }
+  }
